6b4fcc69f639cdbdd1d80cd9c2fdce775e33864c
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-08-04 22:58:28 +0900

    Add: 다마고치 상세 팝업 화면의 Auto Layout 구성

---
+++ Tamagotchi/View/DetailPopupView/DetailPopupViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailPopupViewController.swift
+//  Tamagotchi
+//
+//  Created by Taekwon Lee on 2023/08/04.
+//
+
+import UIKit
+
+class DetailPopupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
