9a0eb3e02e6fe5e83a57cfa120c4c5310e208929
Taekwon Lee <dev.taekwonlee@gmail.com>
2023-08-04 15:23:38 +0900

    Initial Commit

---
+++ Tamagotchi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tamagotchi
+//
+//  Created by Taekwon Lee on 2023/08/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
